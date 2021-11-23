6f2845add100f781377e6a96f4929d5c432fa081
sunaina1904 <60095533+sunaina1904@users.noreply.github.com>
2021-11-23 11:52:10 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,7 @@
-resource "aws_vpc" "my_app" {
+resource "aws_vpc" "javahome_vpc" {
   cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "${var.tenancy}"
-
   tags = {
-    Name        = "JavahomeVpc"
-    Environment = "${terraform.workspace}"
+    Name = "${terraform.workspace}-javahome-vpc"
   }
 }
